666e959eb5fda3c043b930340fe682c381f6e042
KaiDoingCode <80872618+KaiDoingCode@users.noreply.github.com>
2022-11-09 22:32:24 +0200

    Add movie model

---
+++ eTickets/Data/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+namespace eTickets.Data
+{
+	public enum MovieCategory
+	{
+		Action = 1,
+		Comedy,
+		Drama,
+		Documentary
+	}
+}
+
