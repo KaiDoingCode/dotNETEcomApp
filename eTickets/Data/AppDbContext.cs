679383138fcb8dee26bbcc43d1b9c1855cd65d25
KaiDoingCode <80872618+KaiDoingCode@users.noreply.github.com>
2022-11-09 22:43:39 +0200

    Add DbContext

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+namespace eTickets.Data
+{
+	public class AppDbContext : DbContext
+    {
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+		}
+    }
+}
+
