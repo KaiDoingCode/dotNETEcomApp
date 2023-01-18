865c64b906e722e4f6dcbe36409d5d8c78a45818
TuPhung <tu.phung@tuni.fi>
2023-01-18 12:14:43 +0200

    Adding views for Actors, Cinema and Movies and Retouch Navbar

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -19,8 +19,8 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allProducers = await _context.Movies.ToListAsync();
-            return View();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
+            return View(allMovies);
         }
     }
 }
