f88ec903c858ce50e4237ab9ca3b97196062373e
TuPhung <tu.phung@tuni.fi>
2023-01-21 20:27:56 +0200

    added the getallasync and getbyid async

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -8,8 +8,8 @@ namespace eTickets.Data.Services
 {
     public interface IActorsService
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
+        //Task<IEnumerable<Actor>> GetAllAsync();
+        //Task<Actor> GetByIdAsync(int id);
         Task AddAsync(Actor actor);
         Task<Actor> UpdateAsync(int id, Actor newActor);
         Task DeleteAsync(int id);
